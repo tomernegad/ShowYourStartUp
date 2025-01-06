e622091887d14703ff63dc17b386c8204c883eef
Tomer Negad <tomernegad@gmail.com>
2025-01-06 12:55:40 +0200

    Added layers and implement the pink container and heading layers on the home page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,16 @@
 export default function Home() {
   return (
    <>
-     <h1 className='text-2xl'>Home</h1>
+       <section className='pink_container'>
+           <h1 className='heading'>Pitch Your Startup,
+           <br/> Connect With Enterpreneurs
+           </h1>
+              <p className='sub-heading !max-w-3xl' >
+                    Submit your startup to the largest community of enterpreneurs and investors and
+                   Get Noticed in virtual Competitions.
+              </p>
+       </section>
+
    </>
   );
 }
