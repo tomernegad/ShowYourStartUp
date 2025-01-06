54134415a38674cd3f6563579c7d715ed21242d1
Tomer Negad <tomernegad@gmail.com>
2025-01-06 11:17:41 +0200

    Added auth to my app.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/Navbar";
+import React from "react";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+}
