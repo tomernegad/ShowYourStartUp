08bfb346ec16c783758bcd48b13f3bf8559261e6
Tomer Negad <tomernegad@gmail.com>
2025-01-12 10:32:31 +0200

    Now you can find searches depends on catrogry author name and startup name.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,8 +9,8 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
